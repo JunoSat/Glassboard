efa7e7e12147128b5ae1886c83b64b6fbc71fe85
JunoSat <sathvik@avenger@gmail.com>
2026-06-13 11:57:23 +0000

    added remaining schemas of users, handshakes and audit log

--- schema.sql
+++ schema.sql
@@ -12,5 +12,37 @@ create table if not exists tasks (
     title varchar(100) not null,
     is_complete boolean default false,
     module_id int not null,
-    foreign key (module_id) refernces moudules(id) on delete cascade
+    foreign key (module_id) references modules(id) on delete cascade
+) ENGINE=InnoDB;
+
+create table if not exists users(
+    id int auto_increment primary key,
+    username varchar(50) not null unique,
+    password_hashed varchar(255) not null,
+    role enum ('admin', 'manager', 'member') not null default 'member',
+    module_id int,
+    foreign key (module_id) references modules(id) on delete set null
+) ENGINE=InnoDB;
+
+create table if not exists audit_log(
+    id bigint auto_increment primary key,
+    target_table varchar(50) not null,
+    row_id bigint not null,
+    action_type enum('INSERT', 'DELETE', 'UPDATE') not null,
+    old_value text,
+    new_value text,
+    changed_by_user_id int,
+    timestamp datetime default CURRENT_TIMESTAMP,
+    foreign key (changed_by_user_id) references users(id) on delete set null
+) ENGINE=InnoDB;
+
+create table if not exists handshakes(
+    id bigint auto_increment primary key,
+    sender_module_id int not null,
+    receiver_module_id int not null,
+    status enum('PENDING', 'ACTIVE', 'REJECTED') not null default 'PENDING',
+    requested_at datetime default CURRENT_TIMESTAMP,
+    updated_at datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    foreign key (sender_module_id) references modules(id),
+    foreign key (receiver_module_id) references modules(id)
 ) ENGINE=InnoDB;
