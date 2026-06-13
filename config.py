efc3ecb93b7ec40a7afee4bcbb8c3dfb933ace73
JunoSat <sathvik@avenger@gmail.com>
2026-06-13 12:06:10 +0000

    created config files for db access

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'glassboard_db')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'your_fallback_password_here') # change accordingly
+
+
+ # set MYSQL_PASSWORD=your_actual_mysql_password_here in cmd windows
+ # export MYSQL_PASSWORD="your_actual_mysql_password_here" for linux/mac bash/zsh
